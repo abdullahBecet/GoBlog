06bbc446552e78a185146cd7e9def3459ce67fe1
DELL <abdullah.becet@std.hku.edu.tr>
2024-02-10 17:31:15 +0300

    Second step

--- main.go
+++ main.go
@@ -7,10 +7,16 @@ import (
 
 func main() {
 	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/detail", Detail)
 	http.ListenAndServe(":8080", nil)
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	view, _ := template.ParseFiles("index.html")
-	view.Execute(w, nil)
+	view, _ := template.ParseFiles("index.html", "navBar.html")
+	view.ExecuteTemplate(w, "homePage", nil)
+}
+
+func Detail(w http.ResponseWriter, r *http.Request) {
+	view, _ := template.ParseFiles("detail.html", "navBar.html")
+	view.ExecuteTemplate(w, "detail", nil)
 }
